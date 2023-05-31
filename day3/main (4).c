1a3213594bcb3c8cffeacd7c9db7abcb5c1fe9a2
dhruvishaakbari <134709506+dhruvishaakbari@users.noreply.github.com>
2023-05-31 10:56:29 +0530

    Add files via upload

---
+++ day3/main (4).c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+int main(){
+
+int a = 60;
+int b = 45;
+int c = a%b;
+
+    printf("c = %d",c);
+
+    return 0;
+}
---
+++ day3/main.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+int main(){
+
+int a = 12;
+int b = 40;
+int c = a+b;
+
+    printf("c = %d",c);
+
+    return 0;
+}
