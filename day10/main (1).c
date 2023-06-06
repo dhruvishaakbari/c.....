c69138bb60334ddeb97c46e3f34bf3396561c40b
dhruvishaakbari <134709506+dhruvishaakbari@users.noreply.github.com>
2023-06-06 06:40:46 -0700

    Add files via upload

---
+++ day10/main (1).c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    
+    printf("entre:");
+    scanf("%d",&a);
+    
+    printf("entre :");
+    scanf("%d",&b);
+    
+    a>b ? printf("A is max :") : printf("B is max :");
+
+    return 0;
+}
