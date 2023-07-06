dad5e19dae2ad8e20437b615de53c63e7cd0eff3
dhruvishaakbari <134709506+dhruvishaakbari@users.noreply.github.com>
2023-07-06 18:36:46 +0530

    Add files via upload

---
+++ day 29/demo1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ 
+int main(){
+	 
+	 FILE *p;
+	 
+	 char name [20];
+	 
+	 p = fopen("Demo.txt","w");
+	 
+	  if(p == NULL){
+	  	
+	  	printf("error");
+	  	
+	  }
+	  else{
+	  	
+	  	printf("Enter text :");
+	  	
+	  	gets(name);
+	  	
+	  	fputs(name,p);
+	  	fclose(p);
+
+	  }
+	
+	return 0;
+	
+}
---
+++ day 29/demo3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ 
+int main(){
+	 
+	 FILE *p;
+	 
+	 char name [20];
+	 
+	 p = fopen("Demo.txt","a");
+	 
+	  if(p == NULL){
+	  	
+	  	printf("error");
+	  	
+	  }
+	  else{
+	  	
+	  	printf("Enter text :");
+	  	
+	  	gets(name);
+	  	
+	  	fputs(name,p);
+	  	fputs("\n",p);
+	  	fclose(p);
+
+	  }
+	
+	return 0;
+	
+}
